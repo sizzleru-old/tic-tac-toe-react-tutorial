64bad57af8526603d2b5332c15002c62f36de740
sizzleru <xorzong@gmail.com>
2021-12-04 13:55:04 +1100

    Picking a Key

--- src/index.js
+++ src/index.js
@@ -56,6 +56,12 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move#' + move : 'Go to game start';
+            return <li key={move}><button onClick={() => this.jumpTo(move)}>{desc}</button></li>;
+        });
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -70,7 +76,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
